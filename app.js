fdfff8b7def039b325d49d9813f1c730d14ae510
anirudh-tiwari <anirudht2001@gmail.com>
2021-06-23 21:59:53 +0530

    final backend changes

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+var createError = require("http-errors");
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var createError = require('http-errors');
-var express = require('express');
-var cors = require('cors')
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-
-var indexRouter = require('./routes/index');
-var aboutRouter = require('./routes/about');
-var imageRouter = require('./routes/image');
-var productRouter = require('./routes/product');
-var cartRouter = require('./routes/cart');
-var otp = require('./routes/otp');
+var indexRouter = require("./routes/index");
+var aboutRouter = require("./routes/about");
+var imageRouter = require("./routes/image");
+var productRouter = require("./routes/product");
+var cartRouter = require("./routes/cart");
+var otp = require("./routes/otp");
 
 var app = express();
+app.use(cors());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/about', aboutRouter);
-app.use('/image', imageRouter);
-app.use('/product', productRouter);
-app.use('/cart', cartRouter);
-app.use('/otp', otp);
+app.use("/", indexRouter);
+app.use("/about", aboutRouter);
+app.use("/image", imageRouter);
+app.use("/product", productRouter);
+app.use("/cart", cartRouter);
+app.use("/otp", otp);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -46,11 +44,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
